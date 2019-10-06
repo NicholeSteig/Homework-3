7a09e5fb368fd75862b1bd83a5d947163664d364
Nyx <nichole.steig@gmail.com>
2019-10-05 17:41:07 -0700

    created special only array

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var lowChar = confirm("Use lowercase letters?");
 var upChar = confirm("Use uppercase characters?");
 
 var numArray = [];
+var passArray = [];
 
 if (charNum)
 
@@ -24,16 +25,29 @@ if (specChar === true && numChar === false && lowChar === false && upChar === fa
 
     for (var i = 0; i < charNum; i++) {
 
-        var num = Math.floor(Math.random() * 32) + 1;
+        var num = Math.floor(Math.random() * 30) + 1;
 
+        var charArray = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
 
         console.log(num);
+
         
         numArray.push(num);
 
         console.log(numArray);
 
+        
+
+
+            passArray.push(charArray[num]);
+        
+
+        }
+
+        
+
 
     }
 
-}
+    console.log(passArray);
+    
